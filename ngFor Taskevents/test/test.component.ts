7b2793787316bb23b38aa5e6db9aa5a18c556b39
Saurav Sharma <sauravsharma.2001@gmail.com>
2020-02-15 15:04:08 +0530

    ngFor commit

---
+++ ngFor Taskevents/test/test.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl:'./test.component.html' ,
+  
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+  names=["Saurav","Rahul","Juned"]
+ employees=[]
+  
+  constructor() {
+    this.employees=[{"empId":"1","empName":"Saurav","age":22},
+  {"empId":"2","empName":"Rahul","age":52}]
+ }
+
+ onClicked(){
+  this.employees=[{"empId":"1","empName":"Saurav","age":22},
+  {"empId":"2","empName":"Rahul","age":52},
+  {"empId":"3","empName":"Juned","age":32}]
+ }
+ trackByEmpId(emp,index){
+   return emp.empId
+ }
+  ngOnInit(): void {
+  }
+
+}
