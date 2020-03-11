0b36d0cb619111427383e131383ca5ce8154aa7a
Saurav Sharma <sauravsharma.2001@gmail.com>
2020-03-11 17:23:26 +0530

    commit

---
+++ docker_demo/src/main/java/dockerProject/docker_demo/App.java
@@ -0,0 +1,13 @@
+package dockerProject.docker_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
